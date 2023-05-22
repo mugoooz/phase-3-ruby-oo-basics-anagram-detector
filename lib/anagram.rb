e5440620f2137b519727fe489881083e8ff2d22e
Eric Mugo <mugoericpg20@gmail.com>
2023-05-22 15:36:38 +0300

    Finished lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word.downcase
+    end
+
+    def match(possible_anagrams)
+        possible_anagrams.select do |anagram|
+            anagram.downcase.chars.sort == word.chars.sort
+        end
+    end
+    
+end
